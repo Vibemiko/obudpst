88dff626561b83212a7e69d670e1c6be70390b27
Miguel Angel Tarifa <miguel.tarifa@gmail.com>
2026-01-24 09:40:34 -0300

    Added .env.example

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,7 +5,7 @@ import { dirname, join } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config({ path: join(__dirname, '../.env') });
+dotenv.config({ path: join(__dirname, '../../.env') });
 
 export const config = {
   port: process.env.PORT || 3000,
